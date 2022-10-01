f3cc62ab75ce80db6f08fa22688f170d269bcf62
TheLurkerVN <90854642+TheLurkerVN@users.noreply.github.com>
2022-10-01 20:41:30 +0700

    Added play screen UI assets, Spotify fonts. Modified MainActivity.kt to hide title bar

--- app/src/main/java/com/example/musicplayerproject/MainActivity.kt
+++ app/src/main/java/com/example/musicplayerproject/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.musicplayerproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        /*2 lines below will hide the title bar*/
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
+
+        setContentView(R.layout.music_play_screen)
         //cdd
     }
 }
