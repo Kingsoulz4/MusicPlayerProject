b0635d3b1a73a9e934b87114240460cb6bd05164
DatHoang <kingsoulz4a@gmail.com>
2022-10-09 14:53:42 +0700

    Facebook auth

--- app/src/main/java/com/example/musicplayerproject/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/musicplayerproject/activities/SignUpActivity.kt
@@ -8,25 +8,35 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.musicplayerproject.R
 import com.example.musicplayerproject.databinding.ActivitySignUpBinding
+import com.example.musicplayerproject.models.User
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FacebookAuthCredential
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import java.util.*
+
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var signUpRequest: BeginSignInRequest
     private  lateinit var signUpBinding: ActivitySignUpBinding
     private lateinit var signUpClient: GoogleSignInClient
+    private val firebaseDatabase = Firebase.database
+    private  lateinit var callbackManager: CallbackManager
 
-
-    var gso: GoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(R.string.client_id.toString())
-        .requestEmail()
-        .build()
 
     companion object {
         const val SIGN_UP_GOOGLE_REQUEST_CODE = 123
@@ -41,8 +51,55 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(signUpBinding.root)
         supportActionBar?.hide()
 
-        auth = Firebase.auth
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        auth = FirebaseAuth.getInstance()
         signUpClient = GoogleSignIn.getClient(this, gso)
+
+        callbackManager = CallbackManager.Factory.create()
+        LoginManager.getInstance().registerCallback(callbackManager, object : FacebookCallback<LoginResult>{
+            override fun onCancel() {
+
+            }
+
+            override fun onError(error: FacebookException) {
+                Log.i(TAG, "Login fail")
+            }
+
+            override fun onSuccess(result: LoginResult) {
+                Log.i(TAG, "Login successfully")
+                firebaseAuthWithFacebook(result.accessToken)
+            }
+        })
+
+    }
+
+    private fun firebaseAuthWithFacebook(accessToken: AccessToken) {
+        var credential = FacebookAuthProvider.getCredential(accessToken.token)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    Toast.makeText(this, "Facebook auth successfully", Toast.LENGTH_LONG).show()
+                    var user = auth.currentUser;
+                    var uid = user!!.uid
+                    var userName = user!!.displayName
+                    var mail = user!!.email
+                    var profilePicture = user!!.photoUrl.toString()
+
+
+                    var usr = User(uid)
+                    firebaseDatabase.getReference().child("User").child(uid).setValue(usr)
+                    var switchToMainScene = Intent(this, MainMusicPlayerActivity::class.java)
+                    startActivity(switchToMainScene)
+                }
+                else
+                {
+                    Toast.makeText(this, "Facebook auth failed", Toast.LENGTH_LONG).show()
+                }
+
+            }
 
     }
 
@@ -52,26 +109,16 @@ class SignUpActivity : AppCompatActivity() {
     }
 
     fun signUpWithGoogle(view: View) {
-//        signUpRequest = BeginSignInRequest.builder()
-//                .setGoogleIdTokenRequestOptions(
-//                        BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-//                                .setSupported(true)
-//                                // Your server's client ID, not your Android client ID.
-//                                .setServerClientId(getString(R.string.client_id))
-//                                // Only show accounts previously used to sign in.
-//                                .setFilterByAuthorizedAccounts(true)
-//                                .build()
-//                ).build()
-
         var signUpWithGoogleIntent = signUpClient.signInIntent
         startActivityForResult(signUpWithGoogleIntent, SIGN_UP_GOOGLE_REQUEST_CODE)
 
     }
     fun signUpWithFacebook(view: View) {
-
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"))
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager.onActivityResult(requestCode, resultCode, data)
         super.onActivityResult(requestCode, resultCode, data)
 
         if(requestCode == SIGN_UP_GOOGLE_REQUEST_CODE)
@@ -85,12 +132,37 @@ class SignUpActivity : AppCompatActivity() {
             } catch (e: ApiException) {
                 // Google Sign In failed, update UI appropriately
                 Log.w(TAG, "Google sign in failed", e)
+                e.printStackTrace()
             }
         }
     }
 
     private fun firebaseAuthWithGoogle(idToken: String?) {
         Toast.makeText(this, "Login successfully", Toast.LENGTH_LONG).show()
+        var credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this, OnCompleteListener {
+                if (it.isSuccessful)
+                {
+                    Log.i(TAG, "Auth successfully")
+                    var user = auth.currentUser;
+                    var uid = user!!.uid
+                    var userName = user!!.displayName
+                    var mail = user!!.email
+                    var profilePicture = user!!.photoUrl.toString()
+
+                    var usr = User(uid, mail, profilePicture)
+                    firebaseDatabase.getReference().child("User").child(uid).setValue(usr)
+                    var switchToMainScene = Intent(this, MainMusicPlayerActivity::class.java)
+                    startActivity(switchToMainScene)
+                }
+                else
+                {
+                    Log.e(TAG, "Auth fail")
+                    Toast.makeText(this, it.exception!!.message, Toast.LENGTH_LONG).show()
+                }
+
+            })
     }
 
 
---
+++ app/src/main/java/com/example/musicplayerproject/models/User.kt
@@ -0,0 +1,28 @@
+package com.example.musicplayerproject.models
+
+class User {
+    var uid = ""
+    var email = ""
+    var profilePictureURL = ""
+
+    constructor()
+
+    constructor(uid: String?, email: String?, profilePictureURL: String?)
+    {
+        this.uid = uid!!
+        this.email = email!!
+        this.profilePictureURL = profilePictureURL!!
+
+    }
+
+    constructor(uid: String, email: String) {
+        this.uid = uid
+        this.email = email
+    }
+
+    constructor(uid: String) {
+        this.uid = uid
+    }
+
+
+}
