ee562301ab362c091b3207e402dc5da74b61b8da
Dat Hoang <42060490+Kingsoulz4@users.noreply.github.com>
2022-12-11 16:00:41 +0700

    Delete history (#28)

--- app/src/main/java/com/example/musicplayerproject/adapters/HomeItemAdapter.kt
+++ app/src/main/java/com/example/musicplayerproject/adapters/HomeItemAdapter.kt
@@ -5,9 +5,11 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.musicplayerproject.R
@@ -32,7 +34,7 @@ class HomeItemAdapter(
 
 ): RecyclerView.Adapter<HomeItemAdapter.ViewHolder>() {
 
-
+    var sliderContentName = -1
 
     companion object
     {
@@ -114,13 +116,26 @@ class HomeItemAdapter(
             itemView.setOnClickListener {
                 itemClickListener.onItemClicked(position)
             }
+            itemView.findViewById<Button>(R.id.button_delete_item).setOnClickListener{
+                itemClickListener.onDeleteButtonClicked(position)
+            }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(context).inflate(layoutToInflater, parent, false)
-        val layoutParams = view.findViewById<LinearLayout>(R.id.item_home_poster_layout).layoutParams as ViewGroup.MarginLayoutParams
+        val layoutParams = view.findViewById<ConstraintLayout>(R.id.item_home_poster_layout).layoutParams as ViewGroup.MarginLayoutParams
         layoutParams.setMargins(MARGIN, MARGIN, MARGIN, MARGIN)
+
+        if (sliderContentName == R.id.recycle_view_recent)
+        {
+            view.findViewById<Button>(R.id.button_delete_item).visibility = View.VISIBLE
+        }
+        else
+        {
+            view.findViewById<Button>(R.id.button_delete_item).visibility = View.INVISIBLE
+        }
+
         return ViewHolder(view)
     }
 
@@ -132,5 +147,6 @@ class HomeItemAdapter(
 
     interface ItemClickListener {
         fun onItemClicked(position: Int)
+        fun onDeleteButtonClicked(position: Int){}
     }
 }
